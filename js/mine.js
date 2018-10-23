304d1df61d536415990bc4faf4b515f547a16ab3
MohammedSayedAdam <30482644+MohammedSayedAdam@users.noreply.github.com>
2018-10-23 16:08:08 +0200

    Add files via upload

--- js/mine.js
+++ js/mine.js
@@ -1,3 +1,10 @@
-﻿var element = document.getElementById("about");
-
-var myword = document.getElementById('word1');
+﻿
+var element = document.getElementById("barnav");
+var lines = element.getElementsByClassName("nav-item");
+for (var i = 0; i < lines.length; i++) {
+    lines[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
